bb921d41083e6fdbb6bf785759f55c20ba6a973d
guillaume <guillaumechiron4@gmail.com>
2026-08-12 13:50:25 +0200

    modifications fichier init_tables

---
+++ init_tables.py
@@ -0,0 +1,17 @@
+from database.database import engine
+from models.base import Base
+
+# Import des tables dans metadata
+from models.user import User
+from models.client import Client
+from models.contract import Contract
+from models.event import Event
+
+
+def create_tables() -> None:
+    Base.metadata.create_all(bind=engine)
+    print("Tables MySQL créées avec succès.")
+
+
+if __name__ == "__main__":
+    create_tables()
