dc12b0b217c06da8dde6522544d125bad2a2ed63
Guillaume Chiron <guillaumechiron4@gmail.com>
2026-08-21 15:44:22 +0200

    initialisation du fichier

--- test.py
+++ test.py
@@ -1,37 +1,15 @@
 from database.database import SessionLocal
 
+from views.user_view import UserView
 from services.user_service import UserService
 from repositories.user_repository import UserRepository
 
 session = SessionLocal()
-repository = UserRepository(session)
-service = UserService(repository)
+user_view = UserView()
+user_repository = UserRepository(session)
+user_service = UserService(user_repository)
 
-email = input("email: ")
-# password = input("mot de passe: ")
-# first_name = input("prenom: ")
-# last_name = input("nom: ")
-# role = input("role (gestion, support, commercial): ")
+email, password = user_view.login()
+current_user = user_service.authenticate(email, password)
 
-# users = UserRepository(session).users_list()
-# for u in users:
-#     print(u.first_name)
-#     print(u.email)
-#     print("")
-
-# try:
-#     service.create_user(email, password, first_name, last_name, role)
-#     print("user created")
-# except Exception as error:
-#     session.rollback()
-#     print(f"Erreur : {error}")
-# finally:
-#     session.close()
-
-# if service.authenticate(email, password) is not None:
-#     current_user = service.authenticate(email, password)
-#     print("Connecté")
-# else:
-#     print("Mot de passe incorrect")
-
-# print(current_user.first_name)
+user_view.display_user(current_user)
